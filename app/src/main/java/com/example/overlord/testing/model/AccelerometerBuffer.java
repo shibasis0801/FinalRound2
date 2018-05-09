a71eaa455e8e0efdbf5a778c502d590bfeae4f2b
shibasis0801 <shibasis0801@gmail.com>
2018-05-09 14:11:52 +0530

    Testing

---
+++ app/src/main/java/com/example/overlord/testing/model/AccelerometerBuffer.java
@@ -0,0 +1,57 @@
+package com.example.overlord.testing.model;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+/**
+ * Created by overlord on 15/4/18.
+ */
+public class AccelerometerBuffer {
+    private ConcurrentHashMap<Integer, ConcurrentLinkedQueue<float[]>> acceleroValues;
+
+    private float averagedValues[] = new float[3];
+    private long iterationCount = 0;
+
+    public AccelerometerBuffer() {
+        acceleroValues = new ConcurrentHashMap<>();
+
+        for (int i = 0; i < 60; i++)
+            acceleroValues.put(i, new ConcurrentLinkedQueue<>());
+    }
+
+    public void add(int second, float[] values) {
+        acceleroValues.get(second).add(values);
+    }
+
+
+    public float[] getAverage(int second) {
+
+        float[] average = new float[3];
+
+        ConcurrentLinkedQueue<float[]> queue = acceleroValues.get(second);
+
+        float n = (float) queue.size();
+
+        if (queue.isEmpty())
+            return averagedValues;
+
+        else {
+            iterationCount++;
+
+            for (float[] values : queue)
+                for (int i = 0; i < 3; i++)
+                    average[i] += values[i];
+
+            for (int i = 0; i < 3; i++) {
+                average[i] = average[i] / n;
+                averagedValues[i] += (average[i] - averagedValues[i]) / (float)(iterationCount);
+            }
+
+            return average;
+        }
+    }
+
+    public void remove(int second) {
+        acceleroValues.get(second).clear();
+    }
+}
---
+++ app/src/main/java/com/example/overlord/testing/model/MovingWindowBuffer.java
@@ -0,0 +1,64 @@
+package com.example.overlord.testing.model;
+
+
+import com.example.overlord.testing.immutable.CollectionUtils;
+import com.example.overlord.testing.immutable.Consumer;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+
+/**
+ * Created by overlord on 15/4/18.
+ */
+
+public class MovingWindowBuffer <T> {
+    private int count;
+    private int size;
+    private Deque<T> dataBuffer;
+    private boolean has_been_full_once = false;
+    private Consumer<ArrayList<T>> dataBufferConsumer;
+
+
+    public MovingWindowBuffer(int size) {
+        this.size = size;
+        dataBuffer = new ArrayDeque<>();
+    }
+
+    public MovingWindowBuffer<T> add(T data) {
+        //Deque not full
+        if (dataBuffer.size() != size) {
+            dataBuffer.addLast(data);
+        }
+        //Full Deque
+        else {
+            //Deque got full for the first time
+            if ( ! has_been_full_once ) {
+                has_been_full_once = true;
+                runConsumer();
+            }
+
+
+            //Moving Window of T/2 seconds
+            else {
+                dataBuffer.removeFirst();
+                dataBuffer.addLast(data);
+                count++;
+
+                if (count == size / 2) {
+                    count = 0;
+                    runConsumer();
+                }
+            }
+        }
+        return this;
+    }
+
+    private void runConsumer() {
+        dataBufferConsumer.accept(CollectionUtils.dequeToList(dataBuffer));
+    }
+
+    public void setOnBufferFullListener(Consumer<ArrayList<T>> dataBufferConsumer) {
+        this.dataBufferConsumer = dataBufferConsumer;
+    }
+}
