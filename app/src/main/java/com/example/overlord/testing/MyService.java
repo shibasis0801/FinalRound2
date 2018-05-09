505a7c36f60ad1fd9bad46c4c64ec55a20f9a5aa
shibasis0801 <shibasis0801@gmail.com>
2018-05-09 14:16:24 +0530

    Testing

--- app/src/main/java/com/example/overlord/testing/MyService.java
+++ app/src/main/java/com/example/overlord/testing/MyService.java
@@ -1,12 +1,19 @@
 package com.example.overlord.testing;
 
 import android.app.Service;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
 
 
+import com.example.overlord.testing.immutable.AccelerometerSensor;
 import com.example.overlord.testing.immutable.Consumer;
+import com.example.overlord.testing.immutable.TimeUtils;
+import com.example.overlord.testing.model.AccelerometerBuffer;
+import com.example.overlord.testing.model.Fused;
+import com.example.overlord.testing.model.MovingWindowBuffer;
+import com.example.overlord.testing.mutable.Singleton;
 import com.wahoofitness.connector.HardwareConnector;
 import com.wahoofitness.connector.HardwareConnectorEnums;
 import com.wahoofitness.connector.HardwareConnectorTypes;
@@ -16,10 +23,10 @@ import com.wahoofitness.connector.conn.connections.SensorConnection;
 import com.wahoofitness.connector.conn.connections.params.ConnectionParams;
 import com.wahoofitness.connector.listeners.discovery.DiscoveryListener;
 
+import java.util.ArrayList;
 
 
 public class MyService extends Service {
-
 
     private HardwareConnector connector;
     private final HardwareConnector.Listener listener = new HardwareConnector.Listener() {
@@ -33,7 +40,6 @@ public class MyService extends Service {
 
         }
     };
-
     //BlackBox
     public void startDiscovery(Consumer<Heartrate.Data> onNewHeartBeat) {
         connector.startDiscovery(new DiscoveryListener() {
@@ -85,11 +91,76 @@ public class MyService extends Service {
         });
     }
 
+    private Singleton global = Singleton.getInstance();
+
+    private AccelerometerSensor accelerometerSensor;
+    private AccelerometerBuffer accelerometerBuffer;
+    private MovingWindowBuffer<Fused> windowBuffer;
+
+
+    public String getTimeLabel(Heartrate.Data data) {
+        return data.getTime().format("dd_MM_yyyy_HH_mm_ss");
+    }
+
+    public <T> void storeHeartBeat(String time, ArrayList<T> heartBeats) {
+        Log.i("StoreHeartBeatAt", time);
+        global.heartRef
+                .child(time)
+                .setValue(heartBeats)
+                .addOnFailureListener(exception ->
+                        Log.e("Fbase HeartRate Err", exception.getMessage())
+                );
+    }
+
+
+    public Double getHeartBeat(Heartrate.Data data) {
+        return data.getHeartrate().asEventsPerMinute();
+    }
+
+    public void addBeat(Heartrate.Data data) {
+
+        int second = TimeUtils.getSecondsFromTimestamp(data.getTimeMs());
+
+        float []values = accelerometerBuffer.getAverage(second);
+        accelerometerBuffer.remove(second);
+
+        Fused dataPoint = new Fused(getHeartBeat(data), values);
+        Log.i("DataPoint", dataPoint.toString());
+
+        windowBuffer.add(dataPoint)
+                .setOnBufferFullListener(array ->
+                        storeHeartBeat(getTimeLabel(data), array)
+                );
+    }
+
+
     @Override
     public void onCreate() {
         super.onCreate();
         connector = new HardwareConnector(this, listener);
         startDiscovery(heartBeat -> Log.d("", heartBeat.toString()));
+
+
+        windowBuffer = new MovingWindowBuffer<>(60);
+        accelerometerBuffer = new AccelerometerBuffer();
+
+        accelerometerSensor = new AccelerometerSensor(this);
+/*
+        accelerometerSensor.registerListener(
+                sensorValues -> {
+
+                    long timestamp = TimeUtils.getTimeStamp();
+                    int second = TimeUtils.getSecondsFromTimestamp(timestamp);
+
+                    accelerometerBuffer.add(
+                            second,
+                            sensorValues
+                    );
+                }
+        );
+*/
+
+
     }
 
     @Override
